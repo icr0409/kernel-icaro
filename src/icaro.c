6a54b0ba33a24967c20420ba0888b54c9831a5f3
Rapazeutolouco <telesicaro04@gmail.com>
2026-07-11 21:16:36 -0300

    Atualizar o icaro.c

--- src/icaro.c
+++ src/icaro.c
@@ -1,3 +1,15 @@
+
+/*
+ * Kernel-Ícaro (vmicaro)
+ * Copyright (c) 2026 Ícaro Teles da Silva (@icarotelesdasilva)
+ * 
+ * Este código é parte do projeto Kernel-Ícaro.
+ * Licenciado sob a licença GPL v3.
+ * 
+ * É proibida a cópia, modificação ou redistribuição sem 
+ * a manutenção deste aviso de copyright e autoria.
+ */
+
 #include "drivers/vga.c"
 #include "../system/gdt.h"
 
