49a18346e428d4b359860c17d054146fc64bc551
Rapazeutolouco <telesicaro04@gmail.com>
2026-07-12 20:46:01 -0300

    Atualizar o boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,8 +1,5 @@
 ; Kernel-Ícaro (vmicaro)
 ; Copyright (c) 2026 Ícaro Teles da Silva (@icarotelesdasilva)
-; Este código é parte do projeto Kernel-Ícaro.
-; Licenciado sob a licença GPL v3.
-
 
  bits 32
 
