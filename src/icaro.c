740c0341c24a2e6a075a7bd1131ef3dd2eb9bf0b
icarotelesdasilva <telesicaro04@gmail.com>
2026-07-13 18:20:47 -0300

    adicionado ultra simples gerenciamento de memoria

--- src/icaro.c
+++ src/icaro.c
@@ -11,15 +11,48 @@
  */
 
 #include "drivers/vga.c"
-#include "../system/gdt.h"
+#include "../system/gdt.h" 
 
+unsigned char bitmap[5] = {0, 0, 0, 0, 0};
+void  alocar_memoria() {
+for (int i =  0; i < 5;  i++) {
+
+if (bitmap[i] == 0) {
+
+bitmap[i] = 1;
+
+break;
+
+}
+}
+}
+
+void limpar_memoria() {
+
+for (int i = 0; i < 5; i++) {
+
+if (bitmap[i] == 1) {
+
+bitmap[i] = 0;
+
+break;
+
+}
+}
+}
+
+void criar_filtro();
 void inicializar_filtro();
 
-void kmain(void)  {
 
+
+void kmain(void)  {
 gdt_init();
 inicializar_filtro();
 vga_print("Hello, Kernel!");
+
+
+
 
 while(1) {
 
