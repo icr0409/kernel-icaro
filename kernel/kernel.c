a40d0771a7cf313140817f95a0943693efdf9cca
icarotelesdasilva <telesicaro04@gmail.com>
2026-07-17 11:00:27 -0300

    Refactor

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,98 +1,36 @@
-
 /*
  * Kernel-Ícaro (vmicaro)
  * Copyright (c) 2026 Ícaro Teles da Silva (@icarotelesdasilva)
+ *
+ * Licensed under the GPL v3 License.
  */
 
-extern void vga_print(char* str); 
-#include "gdt.h"  
-#include "idt.h" 
+#include "gdt.h"   
+#include "idt.h"   
 #include "pmm.h"
-extern void print_vga(const char* text);
-int inicializar_filtro();
-int limpar_memoria();
 
-unsigned char bitmap[5] = {0, 0, 0, 0, 0};
-void  alocar_memoria() {
-for (int i =  0; i < 5;  i++) {
+extern void vga_print(const char* str);  
 
-if (bitmap[i] == 0) {
-
-bitmap[i] = 1;
-
-break;
-
-}
-}
-}
-
-int  limpar_memoria() {
-
-for (int i = 0; i < 5; i++) {
-	
-if (bitmap[i] == 1) {
-
-bitmap[i] = 0;
-
-break;
-
-}
-}
-}
-
-int criar_filtro();
-int inicializar_filtro();
-
-
-
-void kmain(void)  {
-
-if (gdt_init() !=0) {
-
-vga_print("Kernel Panic!");
-vga_print("\nUnable to load gdt.");
-while(1);
-
-}
-
-
-if (inicializar_filtro() !=0) {
-vga_print("Kernel Panic!");  
-vga_print("\nUnable to load inicializar_filtro."); 
-while(1);
-}
-
-
-
-if (limpar_memoria() !=0) {
-
-
-vga_print("Kernel Panic!");    
-vga_print("\nUnable to load alocar_memoria.");
-while(1);
-
-}
-
-
-    pmm_init(); 
-
-    void* memoria = pmm_alocar_pagina(); 
-
-    if (memoria == 0) {
-        vga_print("Kernel Panic!");      
-        vga_print("\nUnable to load alocar_memoria.");
+void kmain(void) {
+    if (gdt_init() != 0) {
+        vga_print("Kernel Panic!\nUnable to load GDT.");
         while(1);
     }
 
-if (inicializar_idt() != 0) {
-vga_print("Kernel Panic!"); 
-vga_print("\nUnable to load inicializar_idt.");
-while(1);
+    if (inicializar_idt() != 0) {
+        vga_print("Kernel Panic!\nUnable to load IDT.");
+        while(1);
+    }
 
-}
+    pmm_init();  
 
-vga_print("Hello, Kernel!");
+    void* memoria = pmm_alocar_pagina();  
+    if (memoria == 0) {
+        vga_print("Kernel Panic!\nUnable to allocate physical memory page.");
+        while(1);
+    }
+
+    vga_print("Hello, Kernel!");
 
     while (1);
-
 }
